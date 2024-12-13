48811bb91523f792e6b1345a4429a854fe67ed57
Fangyu Gai <greferry@gmail.com>
2024-12-13 18:58:48 +0800

    add keys cmd

--- covenant-signer/cmd/root.go
+++ covenant-signer/cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"path/filepath"
 
 	"github.com/btcsuite/btcd/btcutil"
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/spf13/cobra"
 )
 
@@ -12,17 +13,26 @@ var (
 	configPath    string
 	configPathKey = "config"
 
-	rootCmd = &cobra.Command{
-		Use:   "covenant-signer",
-		Short: "remote signing serivce to perform covenant duties",
-	}
-
 	//   C:\Users\<username>\AppData\Local\signer on Windows
 	//   ~/.signer on Linux
 	//   ~/Library/Application Support/signer on MacOS
 	dafaultConfigDir  = btcutil.AppDataDir("signer", false)
 	dafaultConfigPath = filepath.Join(dafaultConfigDir, "config.toml")
+
+	rootCmd = NewRootCmd()
 )
+
+// NewRootCmd creates a new root command for fpd. It is called once in the main function.
+func NewRootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:               "covenant-signer",
+		Short:             "remote signing service to perform covenant duties",
+		SilenceErrors:     false,
+		PersistentPreRunE: PersistClientCtx(client.Context{}),
+	}
+
+	return cmd
+}
 
 // Execute executes the root command.
 func Execute() error {
