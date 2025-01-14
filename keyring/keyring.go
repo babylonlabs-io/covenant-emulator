8a3e3593dcd62f9f6ace5390f50b27c863bb622a
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-01-14 19:37:23 -0300

    chore: removed unnecessary funcs

--- keyring/keyring.go
+++ keyring/keyring.go
@@ -1,41 +1,14 @@
 package keyring
 
 import (
-	"fmt"
 	"os"
 	"path"
-	"strings"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 
 	"github.com/babylonlabs-io/covenant-emulator/codec"
 	"github.com/babylonlabs-io/covenant-emulator/types"
 )
-
-func CreateKeyring(keyringDir string, chainId string, backend string, input *strings.Reader) (keyring.Keyring, error) {
-	ctx, err := CreateClientCtx(keyringDir, chainId)
-	if err != nil {
-		return nil, err
-	}
-
-	if backend == "" {
-		return nil, fmt.Errorf("the keyring backend should not be empty")
-	}
-
-	kr, err := keyring.New(
-		ctx.ChainID,
-		backend,
-		ctx.KeyringDir,
-		input,
-		ctx.Codec,
-		ctx.KeyringOptions...)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create keyring: %w", err)
-	}
-
-	return kr, nil
-}
 
 func CreateClientCtx(keyringDir string, chainId string) (client.Context, error) {
 	var err error
