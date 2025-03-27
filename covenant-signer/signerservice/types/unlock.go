5463404b18e91c1ebdc0f0e2db62173e403b250e
Vlad <vladislav.varadinov@gmail.com>
2025-03-27 13:22:13 +0200

    CHANGELOG

--- covenant-signer/signerservice/types/unlock.go
+++ covenant-signer/signerservice/types/unlock.go
@@ -1,5 +1,8 @@
 package types
 
+// UnlockRequest represents a request to unlock the key.
+// Note: When using the file keyring backend, the passphrase will be prompted
+// interactively in the terminal, so it doesn't need to be included in this request.
 type UnlockRequest struct {
 	Passphrase string `json:"passphrase,omitempty"`
 }
