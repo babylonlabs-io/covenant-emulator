0aba571a8c06199e3f36d7dd38813c74b6b252ca
Vlad <vladislav.varadinov@gmail.com>
2025-04-02 11:36:04 +0300

    revert e2e test.

--- itest/e2e_test.go
+++ itest/e2e_test.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/babylonlabs-io/covenant-emulator/clientcontroller"
-	"github.com/babylonlabs-io/covenant-emulator/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -66,40 +65,4 @@ func TestQueryPendingDelegations(t *testing.T) {
 	dels, err := tm.CovBBNClient.QueryPendingDelegations(uint64(numDels), nil)
 	require.NoError(t, err)
 	require.Len(t, dels, numDels)
-}
-
-func TestSubmitCovenantSigsWithRetry(t *testing.T) {
-	tm, btcPks := StartManagerWithFinalityProvider(t, 1)
-	defer tm.Stop(t)
-
-	delData := tm.InsertBTCDelegation(t, btcPks, stakingTime, stakingAmount, false)
-	_ = tm.WaitForNPendingDels(t, 1)
-	_ = tm.WaitForNActiveDels(t, 1)
-
-	time.Sleep(10 * time.Second)
-
-	dels := tm.WaitForNVerifiedDels(t, 1)
-
-	// Create two invalid delegations
-	invalidDel1 := &types.Delegation{
-		StakingTxHex: "invalid_hex",
-		BtcUndelegation: &types.Undelegation{
-			UnbondingTxHex: "invalid_unbonding_hex",
-			SlashingTxHex:  "invalid_slashing_hex",
-		},
-	}
-
-	invalidDel2 := &types.Delegation{
-		StakingTxHex: "another_invalid_hex",
-	}
-
-	// Submit a batch with 2 invalid delegations and 1 valid
-	// The retry logic should process only the valid one
-	combinedDels := []*types.Delegation{invalidDel1, invalidDel2, dels[0]}
-	txRespCombined, err := tm.CovenantEmulator.AddCovenantSignatures(combinedDels)
-	require.NoError(t, err, "Combined submission with mixed valid/invalid delegations failed unexpectedly")
-	require.NotNil(t, txRespCombined, "Transaction response should not be nil on successful combined submission")
-	require.NotEmpty(t, txRespCombined.TxHash, "Transaction hash should not be empty for combined submission")
-
-	t.Logf("Successfully submitted batch with mixed valid/invalid delegations, TxHash: %s", txRespCombined.TxHash)
 }
